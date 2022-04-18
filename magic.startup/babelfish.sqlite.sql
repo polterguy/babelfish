7d329a3fe3cb114ca6ecdd76cfb0c507502f26fe
Mohsen Shafiey Kasrineh <mshafiey@users.noreply.github.com>
2022-04-18 11:24:43 +0300

    add SQLite scripts

--- magic.startup/babelfish.sqlite.sql
+++ magic.startup/babelfish.sqlite.sql
@@ -1,1 +1,33 @@
 
+
+
+/*
+* Contains all supported languages in the system.
+*/
+create table languages (
+locale TEXT not null,
+language TEXT null,
+primary key (locale)
+);
+
+/* Inserting default language.
+*/
+insert into languages (locale, language) values ('en', 'English');
+insert into languages (locale, language) values ('it', 'Italian');
+insert into languages (locale, language) values ('es', 'Spanish');
+insert into languages (locale, language) values ('no', 'Norwegian');
+insert into languages (locale, language) values ('fr', 'French');
+/* Contains all translated units in the system.
+*/
+create table translations (
+id TEXT not null,
+locale TEXT not null,
+content text null,
+primary key (id, locale),
+constraint translations_languages_fky foreign key (locale) references languages (locale)
+);
+
+
+
+
+
